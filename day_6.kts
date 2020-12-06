03b13ac12a48cfceaff3183d82ea63c8d08ef186
Pedro Costa <pedro.costa2@sky.uk>
2020-12-06 18:17:27 +0000

    day 6 update

--- day_6.kts
+++ day_6.kts
@@ -3,15 +3,12 @@ import java.io.File
 val groups = File("input6.txt").readText().split("\n\n")
 
 fun day6() {
-    val sol1 = groups.map { it.replace("\n", "") }
-        .map { it.groupBy { it }.count() }
+    groups.map { it.replace("\n", "") }
+        .sumBy { it.groupBy { it }.size }
+        .also { println("Sol1: $it") }
 
-    val sol2 = groups.map { group ->
-        group.replace("\n", "").groupBy { it }
-            .filter { it.value.count() == group.split("\n").size }
-            .count()
-    }
-
-    println("Sol1: ${sol1.sum()}")
-    println("Sol2: ${sol2.sum()}")
+    groups.sumBy { g -> g.trim()
+            .groupBy { it }
+            .filter { it.value.size == g.split("\n").size }.size
+    }.also { println("Sol1: $it") }
 }
